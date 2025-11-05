ac339666c8a3b0bed47a4ecafd17e2c98e8201b1
zhaozhiyong <715740383@qq.com>
2025-11-05 17:28:56 +0800

    2025年10月27日 -add  docker-compose.yaml

--- build/build.sh
+++ build/build.sh
@@ -7,8 +7,19 @@ cd flaskStockCycle
 
 
 
-docker rmi stock:v1.0 -f
-docker build -t stock:v1.0 -f Dockerfile . --progress=plain --no-cache
 
-docker rm -f stock
-docker run -d --name=stock  -p 5010:5000  stock:v1.0
+#
+#docker rmi stock:v1.0 -f
+#docker build -t stock:v1.0 -f Dockerfile . --progress=plain --no-cache
+#
+#docker rm -f stock
+#docker run -d --name=stock  -p 5010:5000  stock:v1.0
+#
+
+
+# 首次部署或重新构建
+docker-compose up -d --build
+
+# 后续启动/停止
+docker-compose start
+docker-compose stop
